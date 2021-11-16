dd5922457f96cb0facc2500d73b0022d9e79e81e
S.a <shanenprz@gmail.com>
2021-11-16 14:27:28 +0000

    code formatting

--- ball-scripts.js
+++ ball-scripts.js
@@ -1,59 +1,58 @@
 window.onload = function () {
-		  // Definitions
-		  var canvas = document.getElementById("key-events-canvas");
-		  var context = canvas.getContext("2d");
-		  
-		  ///ball - free fall
-		  var ball = new Ball(30, "green");
-		  ball.x = canvas.width/2;
-		  ball.y = 50;
-		  ball.context = context;
+	// Definitions
+	var canvas = document.getElementById("key-events-canvas");
+	var context = canvas.getContext("2d");
 
-			var mouseX = 0;
-			var mouseY = 0;
+	///ball - free fall
+	var ball = new Ball(30, "green");
+	ball.x = canvas.width / 2;
+	ball.y = 50;
+	ball.context = context;
 
-			//Mouse Listener
-			canvas.addEventListener ('mousemove', function (event) {
-   		 	var boundings = canvas.getBoundingClientRect();
-				mouseX = event.clientX - boundings.left;
-				mouseY = event.clientY - boundings.top;
-			})
+	var mouseX = 0;
+	var mouseY = 0;
 
-		  animate();
+	//Mouse Listener
+	canvas.addEventListener('mousemove', function (event) {
+		var boundings = canvas.getBoundingClientRect();
+		mouseX = event.clientX - boundings.left;
+		mouseY = event.clientY - boundings.top;
+	})
 
-		  //acceleration
-		  var ax = 0.05;
-		  //gravity
-		  var g = 0.098;
-		  //velocity
-		 
-		  ball.vy = 0; //free fall
-		  
-		  function animate(){
-			
-			reqAnimFrame = window.requestAnimationFrame;
-			reqAnimFrame(animate);
-			
-			// Clear Canvas
-			context.clearRect(0, 0, canvas.width, canvas.height);
-			
-			// Update
-			//free fall
-			ball.y += ball.vy;
-			ball.vy += g;	
+	animate();
 
-			//Mouse Collision Check
-var colMouseX = Math.pow((ball.x - mouseX),2);
-var colMouseY = Math.pow((ball.y - mouseY),2);
-if (Math.sqrt(colMouseX + colMouseY) < ball.r) {
-ball.vy *= -1;
-}
+	//acceleration
+	var ax = 0.05;
+	//gravity
+	var g = 0.098;
+	//velocity
 
-			// Draw
-			ball.draw();
+	ball.vy = 0; //free fall
+
+	function animate() {
+
+		reqAnimFrame = window.requestAnimationFrame;
+		reqAnimFrame(animate);
+
+		// Clear Canvas
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		// Update
+		//free fall
+		ball.y += ball.vy;
+		ball.vy += g;
+
+		//Mouse Collision Check
+		var colMouseX = Math.pow((ball.x - mouseX), 2);
+		var colMouseY = Math.pow((ball.y - mouseY), 2);
+		if (Math.sqrt(colMouseX + colMouseY) < ball.r) {
+			ball.vy *= -1;
 		}
-		  
-		};
 
-	
-	
+		// Draw
+		ball.draw();
+	}
+
+};
+
+
